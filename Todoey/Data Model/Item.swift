0dc47de17b5233611e96fde2511d1911bdf942e8
Jaejun Shin <jaejunshit@gmail.com>
2018-01-07 00:22:57 +0900

    Conver to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jae-Jun Shin on 06/01/2018.
+//  Copyright © 2018 Jae-Jun Shin. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
